e687e8d0053f240dbdc41707bbbf315c42bb0b75
Gant <GantMan@gmail.com>
2015-04-13 23:49:31 -0500

    support for android, and sped up use for iOS

--- lib/newclear.rb
+++ lib/newclear.rb
@@ -1,27 +1,64 @@
 # encoding: utf-8
-
 unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
 
+NUKE_MESSAGE = <<-eos
+   *********************************
+           N U K E  COMPLETE 
+   *********************************
+eos
 
 desc "Completely resets everything for your project"
 task :nuke do
-  puts "\nCleaning Project..."
-  `rake clean:all`
-  puts "\nResetting simulator..."
-  `reset-sim`
-  puts "\nBundling..."
-  `bundle`
-  puts "\nSetting up cocoapods..."
-  `pod setup`
-  puts "\nInstalling cocoapod dependencies..."
-  `rake pod:install`
+  nuke_project
+  puts NUKE_MESSAGE
 end
 
 desc "Completely reset everything in project and run"
 task :newclear do
-  Rake::Task["nuke"].execute
+  # out with the old
+  nuke_project
+  # in with the new
   puts "Building project..."
-  `rake`
+  if is_android?
+    `rake device`
+  else
+    `rake`
+  end
+end
+
+def nuke_project
+  puts "\nCleaning Project..."
+  `rake clean:all`
+  
+  unless is_android?
+    puts "\nResetting simulator..."
+    `reset-sim`
+  end
+
+  puts "\nBundling..."
+  `bundle install`
+  
+  # iOS Depencies
+  if has_task? "pod:install"
+    puts "\nSetting up cocoapods..."
+    `pod setup`
+    puts "\nInstalling cocoapod dependencies..."
+    `rake pod:install`
+  end
+
+  # Android Dependencies
+  if has_task? "gradle:install"
+    puts "\nSetting up gradle automation dependencies..."
+    `rake gradle:install`
+  end
+end
+
+def has_task?(task_name)
+  Rake.application.tasks.count{ |rt| rt.name == "task_name"} > 0
+end
+
+def is_android?
+  @android ||= system("rake -T | grep -q .apk")
 end
